1e73877d8b202ca8d721939a6ff1bc7143be3c4c
Filipe Dos Santos <filipesantos.psic@gmail.com>
2025-06-04 22:07:06 -0300

    prismaClient, ordenation, genres and languages

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,29 @@
-import express from 'express';
+import express from "express";
+// import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
 
-const app = express()
+const app = express();
+const prisma = new PrismaClient();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Apolo!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Apolo!");
+});
 
-app.get("/movies", (req, res) => {
-    res.send("List of movies")
-})
+app.get("/movies", async (_, res) => {
+  const movies = await prisma.movie.findMany({
+    orderBy: {
+      title: "asc",
+    },
+    include: {
+      genres: true,
+      languages: true,
+    },
+  });
+  res.json(movies);
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
